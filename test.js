27342445a09287132415bdfabee5e5934475d980
yunusparvezkhan <yunusparvezkhan@users.noreply.github.com>
2020-12-08 15:27:29 +0530

    First Commit

---
+++ test.js
@@ -0,0 +1,7 @@
+console.log("Hello, This script going to crash your device... I wish you a happy crashing!");
+
+for (let i = 1; i >= 0 ; i+=5) {
+    console.log(i);
+    console.log(i);
+    console.log(i);
+}
